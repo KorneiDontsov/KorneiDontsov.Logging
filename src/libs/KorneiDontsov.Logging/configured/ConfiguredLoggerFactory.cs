91be2f3360a82f03f9d761846389d715f8dfadf5
Kornei Dontsov <KorneiDontsov@gmail.com>
2020-04-21 03:15:16 +0300

    Copy Serilog.Extensions.Logging code and remove dependency. Fix configuring of logger

---
+++ src/libs/KorneiDontsov.Logging/configured/ConfiguredLoggerFactory.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT.
+// See LICENSE in the project root for license information.
+
+namespace KorneiDontsov.Logging {
+	using Microsoft.Extensions.Configuration;
+	using Serilog;
+	using Serilog.Core;
+	using Serilog.Extensions.Logging;
+	using System;
+	using System.Collections.Generic;
+	using static ConfiguredLoggingFunctions;
+
+	class ConfiguredLoggerFactory: SerilogLoggerFactory, IDisposable {
+		public Logger logger { get; }
+
+		ConfiguredLoggerFactory (Logger logger):
+			base(logger) =>
+			this.logger = logger;
+
+		public ConfiguredLoggerFactory
+			(IConfiguration configuration,
+			 IEnumerable<ILoggingProfileApplier> profileAppliers,
+			 IEnumerable<ILoggingEnrichmentApplier> enrichmentAppliers):
+			this(
+				CreateSharedConfiguredLogger(
+					configuration.GetSection("logging"),
+					profileAppliers,
+					enrichmentAppliers)) { }
+
+		void IDisposable.Dispose () {
+			base.Dispose();
+			Log.CloseAndFlush();
+			logger.Dispose();
+		}
+	}
+}
