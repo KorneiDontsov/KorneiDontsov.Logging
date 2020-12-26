e4817cddd90fc4280d861fe6f65859f8482cadca
Kornei Dontsov <KorneiDontsov@gmail.com>
2020-12-27 02:49:08 +0300

    Rework CrashLogger

---
+++ src/libs/KorneiDontsov.Logging/exceptions/NonNativeException.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT.
+// See LICENSE in the project root for license information.
+
+namespace KorneiDontsov.Logging {
+	using System;
+
+	sealed class NonNativeException: Exception {
+		readonly Object exceptionObject;
+
+		public NonNativeException (Object exceptionObject) =>
+			this.exceptionObject = exceptionObject;
+
+		/// <inheritdoc />
+		public override String ToString () => exceptionObject.ToString();
+	}
+}
